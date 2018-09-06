b3eb3456f7d1166d766b1de104f0511bbc0743e6
k1srsufc <k1srsufc@gmail.com>
2018-09-06 13:51:13 +0900

    iot訓練校授業
    C言語のMakefileやC++のお試しファイル・ディレクトリ追加を
    とりあえずコミット

---
+++ ex/hello.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	cout << "Hello C++!\n";
+
+	return 0;
+
+}
+/*ーーーーーーーーーーーーーー
+C++ のお試しコードです。
+初期設定として、
+	#include <iostream>
+	useing namespace std;
+と記述してください。
+
+useing〜　の記述がないと、cout部分の記述を
+	std::cout << "Hello C++!\n";
+と前に std:: を記述しなければならなくなります。
+
+
+Linuxコマンドラインからコンパイルする場合は
+	g++ filename
+を入力してください。
+
+ーーーーーーーーーーーーーー*/
---
+++ ex/makedir/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(void){
+
+	printf("Make!\n");
+	return 0;
+}
